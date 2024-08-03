71601020932b89f9cf4e34454ee82d7d621f24af
Aviv Haruzi <avivharuzi@gmail.com>
2024-08-03 13:46:02 +0300

    rename directory

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/popup',
+  cacheDir: '../../node_modules/.vite/apps/extension',
 
   server: {
     port: 4200,
@@ -25,7 +25,7 @@ export default defineConfig({
   // },
 
   build: {
-    outDir: '../../dist/apps/popup',
+    outDir: '../../dist/apps/extension',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
